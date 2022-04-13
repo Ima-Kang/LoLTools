c04293048adc46db8c210f9a27e4434df36285e8
Ima-Kang <nobody012201@gmail.com>
2022-04-13 17:49:22 -0500

    qss

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile styleSheetFile(":/style/Combinear.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String{styleSheetFile.readAll()};
+    a.setStyleSheet(styleSheet);
     AccountManager w;
     w.show();
     return a.exec();
